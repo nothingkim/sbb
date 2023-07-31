9b035edff535389468979c65598c22ec2642e1cc
nothingkim <hp90047@gmail.com>
2023-07-31 11:55:42 +0900

    apply pagenation

--- src/main/java/com/mygroup/sbb/question/QuestionController.java
+++ src/main/java/com/mygroup/sbb/question/QuestionController.java
@@ -6,14 +6,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.data.domain.Page;
 
 import com.mygroup.sbb.answer.AnswerForm;
+
 import jakarta.validation.Valid;
 import org.springframework.validation.BindingResult;
+
 import lombok.RequiredArgsConstructor;
 
 @RequestMapping("/question")
@@ -24,9 +26,9 @@ public class QuestionController {
     private final QuestionService questionService;
 
     @GetMapping("/list")
-    public String list(Model model) {
-        List<Question> questionList = this.questionService.getList();
-        model.addAttribute("questionList", questionList);
+    public String list(Model model, @RequestParam(value="page", defaultValue="0") int page) {
+        Page<Question> paging = this.questionService.getList(page);
+        model.addAttribute("paging", paging);
         return "question_list";
     }
 
@@ -44,14 +46,10 @@ public class QuestionController {
 
     @PostMapping("/create")
     public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()){
-            if(bindingResult.hasErrors()) {
-                return "question_form";
-            }
+        if (bindingResult.hasErrors()) {
+            return "question_form";
         }
-    
         this.questionService.create(questionForm.getSubject(), questionForm.getContent());
         return "redirect:/question/list"; // 질문 저장후 질문목록으로 이동
-
     }
 }
